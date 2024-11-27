2b199cc84b5ebaa13337587e301435a8f7ed8868
Timnasa <Halisimd212@gmail.com>
2024-11-27 13:15:42 +0300

    Update conversion.js

--- commandes/conversion.js
+++ commandes/conversion.js
@@ -70,7 +70,7 @@ const alea = (ext) => {
     }
 
     sticker = new Sticker(buffer, {
-      pack:"Tkm-Md" ,
+      pack:"TIMNASA-MD" ,
       author: nomAuteurMessage,
       type:
         arg.includes("crop") || arg.includes("c")
@@ -94,7 +94,7 @@ const alea = (ext) => {
     }
 
     sticker = new Sticker(buffer, {
-      pack:"TKM bot", // pack stick
+      pack:"TIMNASA bot", // pack stick
       author:  nomAuteurMessage, // name of the author of the stick
       type:
         arg.includes("-r") || arg.includes("-c")
@@ -252,7 +252,7 @@ zokou({ nomCom: "write", categorie: "Conversion", reaction: "👨🏿‍💻" },
     // Create the sticker
     const stickerMess = new Sticker(meme, {
       pack: nomAuteurMessage,
-      author: 'Tkm-Md',
+      author: 'TIMNASA-MD',
       type: StickerTypes.FULL,
       categories: ["🤩", "🎉"],
       id: "12345",
