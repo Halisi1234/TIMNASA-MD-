893625c5fc6a09a072936aff8c7ee50fc18a60da
Timnasa <Halisimd212@gmail.com>
2024-11-27 16:26:30 +0300

    Update parole.js

--- commandes/parole.js
+++ commandes/parole.js
@@ -18,7 +18,7 @@ zokou({ nomCom: "lyrics",
 
     if (lyrics.error) return repondre("no lyrics found");
 
-    let msg = `---------⬡┃TKM bot┃⬡--------
+    let msg = `---------⬡┃TIMNASA-MD ┃⬡--------
 
 * *Artist :* ${lyrics.artist}
 
