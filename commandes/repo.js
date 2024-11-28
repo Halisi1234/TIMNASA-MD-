9fc9f1ec11478d24ff96c82aadf5548844ac2eea
Timnasa <Halisimd212@gmail.com>
2024-11-28 08:25:44 +0300

    Update repo.js

--- commandes/repo.js
+++ commandes/repo.js
@@ -3,12 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
 zokou({ nomCom: "repo", catégorie:"Général", reaction: "✨", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-  const githubRepo = 'https://github.com/Halisi1234/TIMNASA-MD-';
+  const githubRepo ={` 'https://github.com/Halisi1234/TIMNASA-MD-');
   const img = 'https://files.catbox.moe/ufgvu9.jpg';
 
   try {
     const response = await fetch(githubRepo);
-    const data = await response.json();
+    const data = await response.json(https://github.com/Halisi1234/TIMNASA-MD-);
 
     if (data) {
       const repoInfo = {
@@ -21,13 +21,13 @@ zokou({ nomCom: "repo", catégorie:"Général", reaction: "✨", nomFichier: __f
       const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
       const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
 
-      const gitdata = `┏❏ ⌜ 𝗧𝗜𝗠𝗡𝗔𝗦𝗔-𝐁𝐎𝐓 𝐑𝐞𝐩𝐨 ⌟ ❐
+      const gitdata = `┏❏ ⌜ 𝗧𝗜𝗠𝗡𝗔𝗦𝗔-𝗠𝗗 𝐑𝐞𝐩𝐨 ⌟ ❐
 ┃🗼 *REPOSITORY:* ${data.html_url}
 ┃✨ *STARS:* ${repoInfo.stars}
 ┃🧧 *FORKS:* ${repoInfo.forks}
 ┃📅 *RELEASE DATE:* ${releaseDate}
 ┃🕐 *UPDATE ON:* ${repoInfo.lastUpdate}
-┃👨‍💻 *OWNER* :timotheo
+┃👨‍💻 *OWNER* :TIMNASA-MD 
 ┗❏`;
 
       await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
